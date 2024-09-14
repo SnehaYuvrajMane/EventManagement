8a8cb856654b988dfddb68d6160dde09aade2849
Sneha <snehamane2532@gmail.com>
2024-09-14 19:44:53 +0530

    commit

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -1,10 +1,26 @@
-const express = require('express');
-const { createEvent, getEvents, sendReminder } = require('../controllers/eventController');
+const express = require("express");
+const {
+  createEvent,
+  getEvents,
+  sendReminder,
+} = require("../controllers/eventController");
 const router = express.Router();
 
 // Event Routes
-router.post('/', createEvent);
-router.get('/', getEvents);
-router.post('/:id/reminder', sendReminder);
+router.post("/", createEvent);
+router.get("/", getEvents);
+router.post("/:id/reminder", sendReminder);
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const event = await Event.findByIdAndDelete(req.params.id);
+    if (!event) {
+      return res.status(404).json({ message: "Event not found" });
+    }
+    res.json({ message: "Event deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 module.exports = router;
